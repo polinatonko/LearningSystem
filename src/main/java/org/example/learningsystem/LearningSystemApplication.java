c7a2d7ba3d41f65f08e99b59716f1ded18dc2367
Polina Tonko <p-tonko@mail.ru>
2025-04-24 14:23:53 +0300

    chore: set up initial project structure

---
+++ src/main/java/org/example/learningsystem/LearningSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.learningsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSystemApplication.class, args);
+	}
+
+}
