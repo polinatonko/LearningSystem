cf44ac4e15f62d4a8b51b808c43a088e76b02d94
Polina Tonko <p-tonko@mail.ru>
2025-06-16 15:34:01 +0300

    docs: add javadoc to the xsuaa btp package

--- src/main/java/org/example/learningsystem/btp/xsuaa/util/XsuaaUtils.java
+++ src/main/java/org/example/learningsystem/btp/xsuaa/util/XsuaaUtils.java
@@ -8,11 +8,20 @@ import java.util.Optional;
 
 import static lombok.AccessLevel.PRIVATE;
 
+/**
+ * Utility class for constructing XSUAA URLs.
+ */
 @NoArgsConstructor(access = PRIVATE)
 public class XsuaaUtils {
 
     private static final String XSUAA_URI_TEMPLATE = "https://%s.authentication.us10.hana.ondemand.com";
 
+    /**
+     * Constructs the tenant-specific XSUAA token URL based on the current tenant context.
+     *
+     * @return an {@link Optional} containing the tenant-specific XSUAA token URL if the current tenant is available
+     * in the context
+     */
     public static Optional<String> getTenantTokenUrl() {
         var tenant = TenantContext.getTenant();
         return Optional.ofNullable(tenant)
