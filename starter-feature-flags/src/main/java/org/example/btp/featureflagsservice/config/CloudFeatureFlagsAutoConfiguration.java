cfa0c68e9c3579a3eaa643315ae1eb089078fc76
Polina Tonko <p-tonko@mail.ru>
2025-06-24 15:37:57 +0300

    docs: add javadoc to the starter

--- starter-feature-flags/src/main/java/org/example/btp/featureflagsservice/config/CloudFeatureFlagsAutoConfiguration.java
+++ starter-feature-flags/src/main/java/org/example/btp/featureflagsservice/config/CloudFeatureFlagsAutoConfiguration.java
@@ -7,10 +7,18 @@ import org.example.btp.featureflagsservice.validator.FeatureFlagsValidator;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestClient;
 
+/**
+ * {@link EnableAutoConfiguration Autoconfiguration} for integrating with SAP BTP Feature Flags Service.
+ * <p>
+ * Active by default and can be disabled by settings the property {@code btp.services.feature-flags.enabled} to {@code false}.
+ *
+ * @see LocalFeatureFlagsAutoConfiguration
+ */
 @AutoConfiguration
 @ConditionalOnProperty(name = "btp.services.feature-flags.enabled", havingValue = "true", matchIfMissing = true)
 @EnableConfigurationProperties(FeatureFlagsProperties.class)
@@ -22,13 +30,14 @@ public class CloudFeatureFlagsAutoConfiguration {
     }
 
     @Bean
-    public FeatureFlagsService featureFlagsService(FeatureFlagsProperties featureFlagsProperties, FeatureFlagsValidator featureFlagsValidator, RestClient restClient) {
+    public FeatureFlagsService featureFlagsService(
+            FeatureFlagsProperties featureFlagsProperties, FeatureFlagsValidator featureFlagsValidator, RestClient restClient) {
         return new CloudFeatureFlagsServiceImpl(featureFlagsProperties, featureFlagsValidator, restClient);
     }
+
     @Bean
     @ConditionalOnMissingBean
-    public RestClient featureFlagsRestClient(RestClient.Builder restClientBuilder) {
+    public RestClient restClient(RestClient.Builder restClientBuilder) {
         return restClientBuilder.build();
     }
-
 }
--- starter-feature-flags/src/main/java/org/example/btp/featureflagsservice/config/LocalFeatureFlagsAutoConfiguration.java
+++ starter-feature-flags/src/main/java/org/example/btp/featureflagsservice/config/LocalFeatureFlagsAutoConfiguration.java
@@ -3,9 +3,18 @@ package org.example.btp.featureflagsservice.config;
 import org.example.btp.featureflagsservice.service.FeatureFlagsService;
 import org.example.btp.featureflagsservice.service.LocalFeatureFlagsServiceImpl;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * {@link EnableAutoConfiguration Autoconfiguration} used for local development and testing without a real connection to
+ * the SAP BTP Feature Flags Service.
+ * <p>
+ * Activates by setting the property {@code btp.services.feature-flags.enabled} to {@code false}.
+ *
+ * @see CloudFeatureFlagsAutoConfiguration
+ */
 @AutoConfiguration
 @ConditionalOnProperty(value = "btp.services.feature-flags.enabled", havingValue = "false")
 public class LocalFeatureFlagsAutoConfiguration {
